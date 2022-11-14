57ab9c9be5e48c9c305ecb7b3f15cc4ea22edf6f
paul48avila <116925014+paul48avila@users.noreply.github.com>
2022-11-14 20:05:38 +0000

    intento de grafico

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# hacer que un programa aplique un codigo a las 8 am
-
-import time
-import schedule
-
-def job():
-    print("I'm working...")
-
-# Path: time.py
-# hacer que un programa aplique un codigo a las 8 am
-
-schedule.every().day.at("8:00").do(job) #hacer que un programa aplique un codigo a las 8 am
-while True:
-    
-    schedule.run_pending()
-    time.sleep(1)
+import matplotlib.pyplot as plt
+  
+# year contains the x-axis values
+# and e-india & e-bangladesh
+# are the y-axis values for plotting  
+  
+year = ["I", 'II', "III", "IV", "V","VI"]
+e_india = [100.6, 158.61, 305.54, 394.96, 724.79, 800]
+e_bangladesh = [10.5, 25.21, 58.65, 119.27, 274.87, 800]
+  
+# plotting of x-axis(year) and 
+# y-axis(power consumption)
+#with different colored labels of two countries 
+  
+plt.plot(year, e_india, color ='orange', 
+         label ='Empresa1')
+  
+plt.plot(year, e_bangladesh, color ='g', 
+         label ='Empresa2')
+  
+# naming of x-axis and y-axis
+plt.xlabel('Categorias')
+plt.ylabel('Transacciones')
+  
+# naming the title of the plot
+plt.title('Cantidad de Transacciones por categoria')
+  
+plt.legend()
+plt.show()
