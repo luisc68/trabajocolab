e3ad9bfc9ade4e81bfa7c0652c201054c8ed336c
jeremytooo <117481217+jeremytooo@users.noreply.github.com>
2022-11-14 20:04:50 +0000

    Craetion a csv file, and convert de csv file to pdf.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from datetime import date
 
 
 #(ejemplos y pruebas, ademas de creacion del archivo csv)
+
 with open('test.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     now = datetime.datetime.now()
@@ -21,6 +22,7 @@ with open('test.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         print(row)
+    
 #convertir csv a pdf
 from csv2pdf import convert
 convert("test.csv", "2022-11-15.pdf")
