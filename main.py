4e8d4475ee2f901fd883c043b7cad67447cde7d7
paul48avila <116925014+paul48avila@users.noreply.github.com>
2022-11-14 07:53:51 +0000

    Esta parte le asigna una hora en formato 24 horas para aplicar un codigo, y de base esta definido con una funcion de prueba

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hola mundo")
+# hacer que un programa aplique un codigo a las 8 am
+
+import time
+import schedule
+
+def job():
+    print("I'm working...")
+
+# Path: time.py
+# hacer que un programa aplique un codigo a las 8 am
+
+schedule.every().day.at("8:00").do(job) #hacer que un programa aplique un codigo a las 8 am
+while True:
+    
+    schedule.run_pending()
+    time.sleep(1)
