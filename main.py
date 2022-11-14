89149e8222a90ce91783bf63ccf983bc72370a78
luisc68 <116054815+luisc68@users.noreply.github.com>
2022-11-14 06:46:41 +0000

    Conexion a postgres y obtener datos de la tabla transacciones.

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hola mundo")
+##CONECTAR PYTHON A POSTGRES
+import psycopg2
+
+try:
+    conexion = psycopg2.connect(
+        host = 'localhost',
+        user = 'postgres',
+        password = '1608',
+        database = 'cooperativa'
+    )
+    print('Conexion exitosa')
+    cursor=conexion.cursor()
+    cursor.execute("SELECT version()")
+    row=cursor.fetchone()
+    print(row)
+    cursor.execute("SELECT numerotransaccion, cuentadeorigen, cuentadedestino, monto, fecha FROM transacciones")
+    rows=cursor.fetchall()
+    for row in rows:
+        print(row)
+except Exception as e:
+    print('Error de conexion')
+    print(e)
+finally:
+    conexion.close()
+    print("Conexion finalizada")
