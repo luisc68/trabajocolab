63376c58336dc210422941c8630192d5b0869c00
jeremytooo <117481217+jeremytooo@users.noreply.github.com>
2022-11-14 07:53:39 +0000

    comentarios y un poco de estética.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from datetime import date
 
 
-
+#(ejemplos y pruebas, ademas de creacion del archivo csv)
 with open('test.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     now = datetime.datetime.now()
@@ -22,7 +22,6 @@ with open('test.csv', 'r') as csvfile:
     for row in reader:
         print(row)
 
-now2 = datetime.now()
-
 from csv2pdf import convert
 convert("test.csv", "2022-11-15.pdf")
+
