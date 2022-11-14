754e41807b38879eaf811c8109f3b04f9271877e
jeremytooo <117481217+jeremytooo@users.noreply.github.com>
2022-11-14 07:10:01 +0000

    table csv and convert csv to pdf

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("hola mundo")
+import csv
+import datetime
+import time
+import csv2pdf
+import schedule
+from datetime import datetime
+from datetime import date
+
+
+
+with open('test.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    now = datetime.datetime.now()
+    print(now.strftime("%Y-%m-%d %H:%M:%S"))
+    writer.writerow(['numero de transaccion', '  originaria  ', '  recipiente  ', '  monto  ', 'fecha'])
+    writer.writerow(['          1          ', '  Paul      ', '  5000-uc-12  ', '  100  ', now])
+    writer.writerow(['          2          ', '  Bob       ', '  52040-ll-32  ', '  545 ', now])
+    writer.writerow(['          3          ', '  Jordan    ', '  2345-uuu-2  ', '  1123 ', now])
+
+with open('test.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
+
+now2 = datetime.now()
+
+from csv2pdf import convert
+convert("test.csv", "2022-11-15.pdf")
