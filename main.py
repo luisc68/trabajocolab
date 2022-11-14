b9bf92e73475d2e221a651bdcea9dd49cd343f5b
AidenAlfaro <109189894+AidenAlfaro@users.noreply.github.com>
2022-11-14 14:03:39 -0600

    Transaction graphs

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("THis is a trial")
+from matplotlib import pyplot as plt
+import json
+
+transacciones = json.load(open('data.json', 'r'))
+xAxis = [key for key, value in transacciones.items()]
+yAxis = [value for key, value in transacciones.items()]
+plt.grid(True)
+
+plt.plot(xAxis,yAxis, color='maroon', marker='o')
+plt.xlabel('variable')
+plt.ylabel('value')
+
+fig = plt.figure()
+plt.bar(xAxis,yAxis, color='maroon')
+plt.xlabel('variable')
+plt.ylabel('value')
+
+plt.show()
